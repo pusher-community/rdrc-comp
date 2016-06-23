3c683b83bcc37e538b8f3596c3629f4e3ef73b1a
Ben Foxall <ben@pusher.com>
2016-06-24 06:38:47 +0800

    I’m the winner

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
+require 'dotenv'
 require 'sinatra'
+require 'pusher'
+
+Dotenv.load
+
+client = Pusher::Client.new(
+  app_id: ENV["PUSHER_APP_ID"],
+  key: ENV["PUSHER_APP_KEY"],
+  secret: ENV["PUSHER_APP_SECRET"],
+  cluster: ENV["PUSHER_CLUSTER"],
+  encrypted: true
+)
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
@@ -8,6 +20,9 @@ post '/' do
   if ENV["SECRET"] && ENV["SECRET"] != params["SECRET"]
     403
   else
+    client.trigger('competition', 'winner', {
+      name: 'BEN'
+    })
     200
   end
 end
